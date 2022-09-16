8537b3d44e401502f164152914e528932c0dc833
Rtuta Noguchi <ryuta018@gmail.com>
2022-09-16 10:21:28 +0900

    [add]管理者権限を追加

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>架空日記 | 第2回松本家展</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+    </head>
+    <body class="wrapper" id="diary">
+        <h1>松本家架空日記</h1>
+        <div class='diaries'>
+            @foreach($diaries as $diary)
+                <hr>
+                <div class='diary'>
+                    <h2 class='diary-title'>
+                        <a href="/diary/{{ $diary->id }}">{{ $diary->title }}</a>
+                    </h2>
+                    @if($diary->year < 0)
+                        <h3 class='diary-date'>紀元前{{ -1*$diary->year }}年{{$diary->date}}</h3>
+                    @else
+                        <h3 class='diary-date'>{{ $diary->year }}年{{$diary->date}}</h3>
+                    @endif
+                    <p class='diary-writer'>{{ $diary->user->name }}</p>
+                </div>
+                <form action='/admin/{{ $diary->id }}' method="post" style="display:inline">
+                    @csrf
+                    @method('DELETE')
+                    <button type='submit'>削除</button>
+                </form>
+            @endforeach
+        </div>
+        <hr>
+        <a href="/">戻る</a>
+    </body>
+</html>
